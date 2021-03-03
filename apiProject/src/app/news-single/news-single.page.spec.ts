ac319d8402cf75ddb24c4ae3dc8989f7f6c17cf3
Khumalo Banele Canny <67458707+banelekhumalo@users.noreply.github.com>
2021-03-03 10:37:36 -0800

    cleaning

--- apiProject/src/app/news-single/news-single.page.spec.ts
+++ apiProject/src/app/news-single/news-single.page.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { NewsSinglePage } from './news-single.page';
@@ -7,7 +7,7 @@ describe('NewsSinglePage', () => {
   let component: NewsSinglePage;
   let fixture: ComponentFixture<NewsSinglePage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NewsSinglePage ],
       imports: [IonicModule.forRoot()]
